167b603f0a63947a692362966413106beb45386e
MUkeishBaghel <mukeishbaghel@gmail.com>
2018-04-07 11:30:09 +0530

    first commit Mail asp.net app

---
+++ Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class LoginSignup : System.Web.UI.Page
+{
+    String conn = "Data Source=.\\sqlexpress; Initial Catalog=MailDB; Integrated Security=true";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnlogin_Click(object sender, EventArgs e)
+    {
+        SqlDataAdapter adpt = new SqlDataAdapter("select * from Account", conn);
+        DataSet ds = new DataSet();
+        adpt.Fill(ds);
+
+
+        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+        {
+            if (ds.Tables[0].Rows[i]["User_Name"].ToString() == txtEmail.Text.ToLower() && ds.Tables[0].Rows[i]["Password"].ToString() == txtpassword.Text)
+            {
+                Session["UserName"] = txtEmail.Text;
+                Response.Redirect("~/Inbox.aspx");
+                break;
+            }
+            else
+            {
+                lblMsg.Text = "*Wrong Username Or PassWord";
+            }
+
+
+        }
+    }
+}
